f682d7897d21963d8bac2024ba69ac9a20a03b05
Gregory Cooke <gtcooke94@gmail.com>
2018-05-16 15:13:35 -0400

    Added Nerd Commentor Line

--- ubuntu_install.sh
+++ ubuntu_install.sh
@@ -97,6 +97,7 @@ add_vim_repo 'https://github.com/Shougo/neosnippet.vim.git'
 add_vim_repo 'https://github.com/jlanzarotta/bufexplorer.git'
 add_vim_repo 'https://github.com/lervag/vimtex'
 add_vim_repo 'https://github.com/altercation/vim-colors-solarized.git'
+add_vim_repo 'https://github.com/scrooloose/nerdcommenter.git'
 
 cd $DIR/vimtex
 git checkout master
